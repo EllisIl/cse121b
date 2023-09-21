0987573cb76c2370def255fc64ec886c9483a7e7
Elijah Foard <efoard@byui.edu>
2023-09-21 14:30:09 -0600

    finished w02 task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,26 +4,34 @@
 
 /* Step 2 - Variables */
 let fullName = "Elijah Foard";
-let currentYear = Date().getFullYear();
+let currentYear = new Date().getFullYear();
 let profilePicture = "images/me.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("#Picture");
+const imageElement = document.querySelector("#image");
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent(currentYear);
+yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
 let favFoods = ["Rice", "Eggs", "Pizza", "Beef Stroganoff"]
+
+foodElement.textContent = favFoods.join(", ");
+
 let newFavFood = "Chips"
+favFoods.push(newFavFood);
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;
 
-foodElement.textContent(favFoods);
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;
 
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;
