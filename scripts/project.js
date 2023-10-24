de1074fe27d28b50379386bfa2155f7f5184405a
Elijah Foard <efoard@byui.edu>
2023-10-24 12:35:03 -0600

    test

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,6 @@
 const BASE_URL = "http://api.genius.com";
 const HEADERS = {
-    'Authorization': 'Bearer GdUV96jGSy2-DDaF3EpA0wum9nZSByA7IQKfJaDe_juuQ_WWZf0kwQJoCxGtrnqE'
+    'Authorization': 'Bearer TOKEN'
 };
 const SONG_TITLE = "Lake Song";
 const ARTIST_NAME = "The Decemberists";
